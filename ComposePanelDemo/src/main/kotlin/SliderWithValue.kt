967090b5f2ca66dbe2d1d24cf21c461b3f2e4828
Thomas Künneth <kuennetht@gmail.com>
2024-07-07 14:32:10 +0200

    Switch to Material3

--- ComposePanelDemo/src/main/kotlin/SliderWithValue.kt
+++ ComposePanelDemo/src/main/kotlin/SliderWithValue.kt
@@ -2,9 +2,9 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Slider
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
